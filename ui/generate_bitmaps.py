1b10cab6dc5aea3a282afafed0f06981d9ad341b
Streamliner <jimmy.xrail@me.com>
2019-11-12 10:49:15 -0600

    LCD user interface design and script

---
+++ ui/generate_bitmaps.py
@@ -0,0 +1,43 @@
+import png
+from pathlib import Path
+
+
+def convert_line(all_bytes):
+    result = []
+    counter = 0
+    buffer = 0
+    for b in all_bytes:
+        if counter == 8:
+            result.append(buffer)
+            counter = 0
+            buffer = 0
+        buffer *= 2
+        if b:
+            buffer += 1
+        counter += 1
+    if counter:
+        result.append(buffer)
+    return result
+
+
+def convert_image(path):
+    f = open(path, 'rb')
+    r = png.Reader(f)
+    width, height, rows, _ = r.read()
+    result = []
+    for row in rows:
+        result.extend(convert_line(row))
+    name = 'UI_'+Path(path).name.split('.')[0].upper()
+    s = 'const uint8_t %s [] PROGMEM = {%s};\n' % (
+        name, ', '.join(map(str, result))
+    )
+    print('Exported: %s' % name)
+    return s
+
+
+fdata = ['#pragma once\n', '#include <Arduino.h>\n', '\n'];
+for i in Path('.').glob('*.png'):
+    fdata.append(convert_image(i))
+
+with open('../src/lcd_ui.h', 'w') as fp:
+    fp.writelines(fdata)
